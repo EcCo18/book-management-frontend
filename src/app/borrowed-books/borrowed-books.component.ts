af67654ac3eed56160555cdff404e78f1584d0de
EcCo18 <clemens.kurzeya@icloud.com>
2022-09-26 10:48:37 +0200

    feat(home): added component for home

--- src/app/borrowed-books/borrowed-books.component.ts
+++ src/app/borrowed-books/borrowed-books.component.ts
@@ -1,5 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book {
+  name: string;
+  isbn: string;
+  borrowDate: number;
+  author: string;
+}
+
+const COUNTRIES: Book[] = [
+  {
+    name: 'Building Microservices',
+    isbn: '4354356564',
+    borrowDate: 2022,
+    author: "146989754"
+  },
+  {
+    name: 'Canada',
+    isbn: '345345345345',
+    borrowDate: 9976140,
+    author: "36624199"
+  },
+  {
+    name: 'United States',
+    isbn: '345643653',
+    borrowDate: 9629091,
+    author: "324459463"
+  },
+  {
+    name: 'China',
+    isbn: '43543543543',
+    borrowDate: 9596960,
+    author: "1409517397"
+  }
+];
+
 @Component({
   selector: 'app-borrowed-books',
   templateUrl: './borrowed-books.component.html',
@@ -7,9 +41,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BorrowedBooksComponent implements OnInit {
 
-  constructor() { }
+  countries = COUNTRIES;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
