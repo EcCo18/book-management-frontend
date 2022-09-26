aed365eb59247e3ebb5f69cbfcbaeef7d0436f22
EcCo18 <clemens.kurzeya@icloud.com>
2022-09-26 12:21:50 +0200

    feat(borrowed-books-component): borrowed books shows now all available books

--- src/app/components/borrowed-books/borrowed-books.component.ts
+++ src/app/components/borrowed-books/borrowed-books.component.ts
@@ -1,33 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import Book from "../../models/Book";
 import { BookService } from "../../services/book-service/book.service";
-
-const BOOKS: Book[] = [
-  {
-    name: 'Building Microservices',
-    isbn: '4354356564',
-    borrowDate: 2022,
-    author: "146989754"
-  },
-  {
-    name: 'Route 66',
-    isbn: '345345345345',
-    borrowDate: 2001,
-    author: "36624199"
-  },
-  {
-    name: 'United States',
-    isbn: '345643653',
-    borrowDate: 1978,
-    author: "324459463"
-  },
-  {
-    name: 'Die zweite Legion',
-    isbn: '43543543543',
-    borrowDate: 2022,
-    author: "1409517397"
-  }
-];
+import { Observable } from "rxjs";
+import BookDto from "../../models/dtos/BookDto";
 
 @Component({
   selector: 'app-borrowed-books',
@@ -36,12 +10,12 @@ const BOOKS: Book[] = [
 })
 export class BorrowedBooksComponent implements OnInit {
 
-  books = BOOKS;
+  books$: Observable<BookDto[]>;
 
   constructor(private bookService: BookService) {
+    this.books$ = this.bookService.getAllBooks();
   }
 
   ngOnInit(): void {
-    this.bookService.getAllBooks().subscribe(books => books.forEach(book => console.log(book)));
   }
 }
