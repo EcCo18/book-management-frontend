fa8f46ae1a47bc367c33b1a79625130e64267adb
EcCo18 <clemens.kurzeya@icloud.com>
2022-09-26 10:34:56 +0200

    feat(components): created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { BorrowComponent } from './borrow/borrow.component';
+import { BorrowedBooksComponent } from './borrowed-books/borrowed-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AdminComponent,
+    BorrowComponent,
+    BorrowedBooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
